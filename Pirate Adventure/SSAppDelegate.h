c89731aa703828c51e531c078047ee44f4274f6f
Sawant Shah <sawant@sawantshah.com>
2014-09-24 12:18:44 +0500

    Initial Commit

---
+++ Pirate Adventure/SSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SSAppDelegate.h
+//  Pirate Adventure
+//
+//  Created by Sawant Shah on 24/09/2014.
+//  Copyright (c) 2014 Sawant Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pirate Adventure/SSViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSViewController.h
+//  Pirate Adventure
+//
+//  Created by Sawant Shah on 24/09/2014.
+//  Copyright (c) 2014 Sawant Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSViewController : UIViewController
+
+@end
